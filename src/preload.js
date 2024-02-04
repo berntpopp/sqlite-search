bf0e3a65bbde6247963a79a457a28162b10efc97
Bernt Popp <bernt.popp@gmail.com>
2024-02-04 10:02:42 +0100

    feat: enhance search functionality and UI feedback
    Implemented a dropdown in App.vue for selecting which database table to search in. Added search term input field and magnify icon for initiating the search. Refined the Details dialog for a more user-friendly display of search results. Introduced a snackbar component for user notifications and feedback. Enhanced the preload.js to include IPC communication for table and column listings. Updated background.js to handle dynamic queries based on selected table and to only list FTS5 tables.
    
    Closes #2

--- src/preload.js
+++ src/preload.js
@@ -2,7 +2,15 @@
 
 const { contextBridge, ipcRenderer } = require('electron');
 
+/**
+ * Exposes a safe, limited API to the renderer process, providing functionality
+ * for performing database searches and retrieving database information.
+ */
 contextBridge.exposeInMainWorld('electronAPI', {
-  performSearch: (searchTerm) => ipcRenderer.send('perform-search', searchTerm),
-  onSearchResults: (callback) => ipcRenderer.on('search-results', callback)
+  performSearch: (searchTerm, selectedTable) => ipcRenderer.send('perform-search', searchTerm, selectedTable),
+  onSearchResults: (callback) => ipcRenderer.on('search-results', callback),
+  getTableList: () => ipcRenderer.send('get-table-list'),
+  onTableList: (callback) => ipcRenderer.on('table-list', callback),
+  getColumns: (tableName) => ipcRenderer.send('get-columns', tableName),
+  onColumnsList: (callback) => ipcRenderer.on('column-list', callback),
 });
